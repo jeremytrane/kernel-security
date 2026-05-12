9de3d304cc4c7bf90a68f48f41bcd46d88e45b0c
jeremytrane <114171300+jeremytrane@users.noreply.github.com>
2026-05-12 20:46:59 +0100

    Addresses and values of a pointer

--- Advanced C Programming Pointers/test.c
+++ Advanced C Programming Pointers/test.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-#define LENGTH 3
+#define LENGTH 5
 
-int data[LENGTH];
+int data[LENGTH] = {
+    20, 22, 24, 26, 28
+};
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
 
     int *pi;
     int **ppi;
 
-    for (int i = 0; i < LENGTH ; i++) {
-        data[i] = i;
-    }
-
-    for (int i = 0; i < LENGTH ; i++) {
-        // printf("%d\n", data[i]);
+    for (int i = 0; i < LENGTH; i++) {
+        printf("%d, ", data[i]);
     }
 
     pi = data;
-    printf("%d\n", *pi);
+    printf("\nValue of what pi points to: %d\n", *pi);
+    printf("Address of what pi points to: %d\n", pi);
+    printf("Addres of pi: %d\n", &pi);
 
     return 0;
 }
