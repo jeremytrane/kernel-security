bbab9f81f5640f6291e60d2807695f3613a53a7b
jeremytrane <114171300+jeremytrane@users.noreply.github.com>
2026-06-02 21:13:58 +0100

    Full course worth of ntoes

---
+++ Advanced C Programming Pointers/master.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define LENGTH 5
+
+int data[LENGTH];
+
+int main(int argc, char** argv) {
+
+    for (int i = 0; i < LENGTH; i++) {
+        data[i] = 20 + (i*2);
+    }
+
+    int *pi;
+    int **ppi;
+
+    for (int i = 0; i < LENGTH; i++) {
+        printf("%d, ", data[i]);
+    }
+
+    pi = data;
+    printf("\nValue of what pi points to: %d\n", *pi);
+    printf("Address of what pi points to: %p\n", (void*)pi);   // line 20
+    printf("Address of pi: %p\n", (void*)&pi);                 // line 21
+
+    ppi = &pi;
+    printf("\nValue of what ppi points to: %p\n", (void*)*ppi);  // line 24 — this is pi's value (addr of data)
+    printf("Address of what ppi points to: %p\n", (void*)ppi); // line 25
+    printf("Address of ppi: %p\n", (void*)&ppi);               // line 26
+
+    return 0;
+}
